d0ab98b24cb35de11f49e9bef2f957ac5fe701ea
Brian Hession <github@bhmail.me>
2023-10-17 13:24:53 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc = include_str!("../README.md")]
+#![forbid(unsafe_code)]
+#![warn(
+    clippy::mod_module_files,
+    clippy::unwrap_used,
+    rust_2018_idioms,
+    unused_lifetimes,
+    unused_qualifications
+)]
+
+extern crate alloc;
+
+mod basic;
+mod request;
+mod response;
+
+pub mod builder;
+pub mod ext;
+
+pub use basic::{
+    BasicOcspResponse, CertId, CertStatus, KeyHash, ResponderId, ResponseData, RevokedInfo,
+    SingleResponse, UnknownInfo, Version,
+};
+pub use request::{OcspRequest, Request, Signature, TbsRequest};
+pub use response::{OcspNoCheck, OcspResponse, OcspResponseStatus, ResponseBytes};
