cb39d0c5a0363d574cdd59e153472e6b3ad2f9ca
bhesh <github@bhmail.me>
2024-01-09 10:00:57 -0500

    deprecated

--- target/debug/deps/ocsp_x509-3f3feb54d4417851.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Brian\Documents\Workspace\ocsp-x509\target\debug\deps\ocsp_x509-3f3feb54d4417851.rmeta: src/lib.rs
-
-C:\Users\Brian\Documents\Workspace\ocsp-x509\target\debug\deps\libocsp_x509-3f3feb54d4417851.rlib: src/lib.rs
-
-C:\Users\Brian\Documents\Workspace\ocsp-x509\target\debug\deps\ocsp_x509-3f3feb54d4417851.d: src/lib.rs
-
-src/lib.rs:
